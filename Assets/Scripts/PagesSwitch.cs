564dc4288e02d947b7cc744064511835f6ac4e94
ifiGAIA <39110447+ifiGAIA@users.noreply.github.com>
2021-11-07 14:55:04 +0800

    20211107_finished

--- Assets/Scripts/PagesSwitch.cs
+++ Assets/Scripts/PagesSwitch.cs
@@ -7,15 +7,18 @@ public class PagesSwitch : MonoBehaviour
 {
     public GameObject pageinit;
     public GameObject page1;
-    public GameObject page2;
+    public GameObject[] page2;
     public bool p1;
     public bool p2;
     public int time;
     // Start is called before the first frame update
     void Start()
     {
-        // page1.SetActive(true);
-        // page2.SetActive(false);
+        page1.SetActive(true);
+        for(int i=0; i<4; i++)
+        {
+            page2[i].SetActive(false);
+        }
         p1 = false;
         p2 = false;
         Time();
@@ -33,16 +36,49 @@ public class PagesSwitch : MonoBehaviour
     void InitPageToPage1()
     {
         pageinit.SetActive(false);
-        // page1.SetActive(true);
+        page1.SetActive(true);
         p1 = true;
     }
-    public void SwitchtonextPages()
+    public void SwitchtoPages_Margaret()
     {
         //第一頁去第二頁
         if(p1 == true && p2 == false)
         {
             page1.SetActive(false);
-            page2.SetActive(true);
+            page2[0].SetActive(true);
+            p1 = false;
+            p2 = true;
+        }
+    }
+    public void SwitchtoPages_Hawaii()
+    {
+        //第一頁去第二頁
+        if(p1 == true && p2 == false)
+        {
+            page1.SetActive(false);
+            page2[1].SetActive(true);
+            p1 = false;
+            p2 = true;
+        }
+    }
+    public void SwitchtoPages_Seafood()
+    {
+        //第一頁去第二頁
+        if(p1 == true && p2 == false)
+        {
+            page1.SetActive(false);
+            page2[2].SetActive(true);
+            p1 = false;
+            p2 = true;
+        }
+    }
+    public void SwitchtoPages_Vegetable()
+    {
+        //第一頁去第二頁
+        if(p1 == true && p2 == false)
+        {
+            page1.SetActive(false);
+            page2[3].SetActive(true);
             p1 = false;
             p2 = true;
         }
@@ -50,10 +86,13 @@ public class PagesSwitch : MonoBehaviour
     public void SwitchtoprePages()
     {
         //第二頁去第一頁
-        if(p2 == true && p1 == false)
+        if(p1 == false)
         {
-            page2.SetActive(false);
             page1.SetActive(true);
+            for(int i=0; i<4; i++)
+            {
+                page2[i].SetActive(false);
+            }
             p2 = false;
             p1 = true;
         }
